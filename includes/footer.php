7c9eb3d3b1f5bb475a5d03b3646025efe6405ddd
SAJAL <72509355+sxjal@users.noreply.github.com>
2022-12-16 03:45:21 +0530

    Added all the assets used in the project

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2022 Online Sports Inventory Management System |<a href="" target="_blank"  > Designed by : Rohit Kumar & Sajal Sahu</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ includes/header.php
@@ -0,0 +1,68 @@
+<div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">
+
+                    <img src="assets/img/logo.png" />
+                </a>
+
+            </div>
+
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="dashboard.php" class="menu-top-active">DASHBOARD</a></li>
+                           
+                            <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Categories <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-category.php">Add Category</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-categories.php">Manage Categories</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Distributor <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-distributor.php">Add Distributor</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-distributors.php">Manage Distributor</a></li>
+                                </ul>
+                            </li>
+ <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Sport Items <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-item.php">Add Items</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-items.php">Manage Items</a></li>
+                                </ul>
+                            </li>
+
+                           <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Issue Items <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="issue-item.php">Issue New Items</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-issued-items.php">Manage Issued Items</a></li>
+                                </ul>
+                            </li>
+                             <li><a href="reg-students.php">Reg Students</a></li>
+                    
+  <!-- <li><a href="change-password.php">Change Password</a></li> -->
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
